57c40a1329ec57d030eecc9382a8f37765e38ecb
tzivadinovic <zivadinovictomislav@gmail.com>
2021-10-16 01:43:34 +0200

    added swagger info

--- src/main/java/rs/carrot/backend/config/SwaggerConfig.java
+++ src/main/java/rs/carrot/backend/config/SwaggerConfig.java
@@ -3,8 +3,10 @@ package rs.carrot.backend.config;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
+import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.PathSelectors;
 import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.ApiInfo;
 import springfox.documentation.spi.DocumentationType;
 import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
@@ -19,6 +21,18 @@ public class SwaggerConfig {
                 .select()
                 .apis(RequestHandlerSelectors.any())
                 .paths(PathSelectors.any())
+                .build()
+                .apiInfo(apiInfo());
+    }
+
+    ApiInfo apiInfo() {
+        return new ApiInfoBuilder()
+                .title("Carrot API")
+                .description("Carrot Online Shop API documentation")
+                .license("Apache 2.0")
+                .licenseUrl("http://www.apache.org/licenses/LICENSE-2.0.html")
+                .termsOfServiceUrl("")
+                .version("1.0.0")
                 .build();
     }
 }
